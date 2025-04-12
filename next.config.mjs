ee1d4fee1233768dc9d6dc4da791a0b1c8b7f21a
Gan-3rdene <ganerdenegansukh8@gmail.com>
2025-04-12 20:38:29 +0800

    AAAAAAAAAAAAAAA

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
   basePath: '/whatever',
   assetPrefix: '/whatever/',
-}
+};
 
 export default nextConfig;
