c107c140970d3ca2fa90c10510dd7f88e4380449
Gan-3rdene <ganerdenegansukh8@gmail.com>
2025-05-02 11:41:39 +0800

    Database

--- src/pages/reusable/loginform.js
+++ src/pages/reusable/loginform.js
@@ -4,9 +4,46 @@ import { useState } from 'react';
 // import Footer from "./footer.js";
 
 export default function LoginForm() {
+  const [showSignup, setShowSignup] = useState(false);
+
   const ChangeToMain = () => {
     window.location.replace("/whatever");
   };
+  
+  const handleSignupClick = () => {
+    if(showSignup == false){
+      setShowSignup(true);
+    }
+  }
+  const handleSignup = async () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email")?.value;
+    const phone = document.getElementById("phone")?.value;
+    try {
+      const response = await fetch("http://localhost:5002/userpost", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password, email, phone }),
+      });
+      const data = await response.json();
+      if(response.ok) {
+        // setUsername(username);
+        localStorage.setItem("username", username);
+        ChangeToMain();
+      }
+      else {
+        console.error(data.message);
+        alert(data.message);
+      }
+    }
+    catch(error) {
+      console.error("Error, ", error);
+    }
+  };
+
   const handleLogin = async () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -51,9 +88,25 @@ export default function LoginForm() {
             <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" />
           </div>
+          {showSignup && (
+            <> 
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="phone">Phone number</label>
+                <input type="text" id="phone" name="phone" />
+              </div>
+            </>
+          )}
 
           <div className={styles.buttonGroup}>
-            <button className={styles.button}>Sign up</button>
+            {!showSignup ? (
+              <button className={styles.button} onClick={handleSignupClick}>Sign up</button>
+            ) : (
+              <button className={styles.button} onClick={handleSignup}>Submit</button>
+            )}
             <button className={styles.button} onClick={handleLogin}>Log in</button>
           </div>
 
